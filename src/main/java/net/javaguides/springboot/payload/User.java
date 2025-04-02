3bdc382e9816ae920a7bf3ce12a8af67ed50a59d
neerajkumarkannoujiya <neerajkumar7388011@gmail.com>
2025-04-02 21:01:47 +0530

    Here the new kafka consumer and producer code using springboot

---
+++ src/main/java/net/javaguides/springboot/payload/User.java
@@ -0,0 +1,42 @@
+package net.javaguides.springboot.payload;
+
+import org.apache.kafka.common.protocol.types.Field;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
